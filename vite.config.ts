46b28b7a08e57b748cec2c3f38f9e158bb9298c5
John Forster <john.forster@makers.tech>
2025-10-25 20:08:30 +0100

    Update for unified deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import preact from "@preact/preset-vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: "/botc-script-checker",
-  base: "https://johnforster.github.io/botc-script-checker",
+  base: "/",
   plugins: [preact()],
 });
